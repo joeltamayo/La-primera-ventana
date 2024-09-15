3fa3c7392cd3f902ed0ee6b54e3e16b3af3972eb
joeltamayo <jueltamayo25@gmail.com>
2024-09-14 18:56:35 -0600

    2.6. El flujo de la interfaz

--- Assets/Scripts/ControladordeColision.cs
+++ Assets/Scripts/ControladordeColision.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ControladordeColision : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D coll){
-        Debug.Log("La antorcha toco la gogata");
+    public Animator animator; // Referencia al Animator
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Debug.Log("La antorcha toco la fogata");
+        animator.SetBool("IsEncendida", true); // Cambia el parámetro a true
     }
 }
